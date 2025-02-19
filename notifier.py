9bf15174e32d3e77630cf8a12ae8374935bf273b
Aluisio Lucio <aluisio18lucio@gmail.com>
2025-02-19 12:22:42 -0300

    Correction in the abstract method

--- notifier.py
+++ notifier.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 class Notifier(ABC):
     @abstractmethod
     def notify(self, message: str):
-        NotImplementedError
+        raise NotImplementedError
 
 
 # Implementações concretas do Notifier (Herança e Polimorfismo)
